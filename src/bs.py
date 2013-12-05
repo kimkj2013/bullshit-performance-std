599b125f976797ee747c9587a57ec95344de21af
Nate Mara <natemara@gmail.com>
2013-12-04 22:49:47 -0500

    changed seconds to milliseconds

--- src/bs.py
+++ src/bs.py
@@ -6,13 +6,16 @@ import os
 
 count = 0
 start_time = time.time()
+
 f = open("bs", "w")
 
 while count <= 50000000:
     f.write("bullshit")
     count += 1
 
-print time.time() - start_time
+final_time = time.time() - start_time
+
+print int(final_time * 1000)
 
 f.close()
 os.remove("bs")
