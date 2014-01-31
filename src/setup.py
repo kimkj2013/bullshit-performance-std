4c68f97deb7a271729c3a1776725bec55e67740a
Nate Mara <natemara@gmail.com>
2014-01-30 21:36:08 -0500

    Updated setup scripts so that python3 binary is made

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,17 @@
-from distutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
 
-setup(console=['bullshit.py'])
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = ["os", "argparse", "time"], excludes = [])
+
+base = 'Console'
+
+executables = [
+    Executable('bullshit.py', base=base, targetName = 'bullshit.exe')
+]
+
+setup(name='The Bullshit Performance Standard',
+      version = '0.5.0',
+      description = 'Easily benchmark your computer using bullshit!',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
