78dd4b3c234c26743d20e1a57a9e1d705c940f41
Nate Mara <natemara@gmail.com>
2014-02-21 22:42:43 -0500

    reformatted for pep compliance

--- src/bullshit.py
+++ src/bullshit.py
@@ -33,7 +33,9 @@ version_major = 0
 version_minor = 6
 version_patch = 0
 
-version = str(version_major) + "." + str(version_minor) + "." + str(version_patch)
+version = str(version_major) + "."
+version += str(version_minor)
+version += "." + str(version_patch)
 
 license_info = """
 Copyright 2014 Nathan Mara
@@ -43,9 +45,10 @@ All outputted bullshit licensed under CC BY-NC
 
 max = 50000000
 
+
 def main():
 	"""
-	
+
 	"""
 
 	global write_file
@@ -76,7 +79,7 @@ def main():
 			one_tenth_write(f, bullshit_string)
 
 		if not args.terse:
-			print(str((i+1) * 10) + "%")
+			print(str((i + 1) * 10) + "%")
 
 	if write_file:
 		final_time = time.time() - start_time
@@ -88,6 +91,7 @@ def main():
 	if not args.keep and write_file:
 		os.remove(filename)
 
+
 def setup_argparse():
 	"""
 	sets up the objects necessary for argparse to parse the command line arguments
@@ -96,18 +100,29 @@ def setup_argparse():
 	global parse
 	global args
 
-	parse = argparse.ArgumentParser(description="Measure your computer's performance with bullshit!")
+	parser_description = "Measure your computer's performance with bullshit!"
+
+	help_verbose = "Prints all of the bullshit to the console."
+	help_verbose += " Not recommended - very, very slow."
+	help_terse = "Allow only the score to be written to the console"
+	help_keep = "Keeps the \"%s\" file for your use" % filename
+	help_newline = "Uses a newline character after each word"
+	help_force = "Forces the program to write the \"%s\" file."
+	help_force += " Use this with verbose flag." % filename
+
+	parse = argparse.ArgumentParser(description=parser_description)
 
 	verbosity = parse.add_mutually_exclusive_group()
 
-	verbosity.add_argument("-v", "--verbose", action="store_true", help="Prints all of the bullshit to the console. Not recommended - very, very slow.")
-	verbosity.add_argument("-t", "--terse", action="store_true", help="Allow only the score to be written to the console")
+	verbosity.add_argument("-v", "--verbose", action="store_true", help=help_verbose)
+	verbosity.add_argument("-t", "--terse", action="store_true", help=help_terse)
 
-	parse.add_argument("-k", "--keep", action="store_true", help="Keeps the \"{0}\" file for your use".format(filename))
-	parse.add_argument("-n", "--newline", action="store_true", help="Uses a newline character after each word")
-	parse.add_argument("-f", "--force", action="store_true", help="Forces the program to write the \"{0}\" file. Use this with verbose flag.".format(filename))
+	parse.add_argument("-k", "--keep", action="store_true", help=help_keep)
+	parse.add_argument("-n", "--newline", action="store_true", help=help_newline)
+	parse.add_argument("-f", "--force", action="store_true", help=help_force)
 
 	args = parse.parse_args()
+
 
 def one_tenth_write(file, bullshit):
 	"""
@@ -117,10 +132,11 @@ def one_tenth_write(file, bullshit):
 	for i in range(0, loop_size):
 		file.write(ascii(bullshit))
 
+
 def one_tenth_print(bullshit):
 	"""
-	Prints one-tenth of the specified bullshit to the screen. If the file is
-	to be written, specified by the command line arguments, then it is.
+	Prints one-tenth of the specified bullshit to the screen. If the file is to
+	be written, specified by the command line arguments, then it is.
 	"""
 	if write_file:
 		one_tenth_write(f, bullshit)
@@ -128,6 +144,7 @@ def one_tenth_print(bullshit):
 	loop_size = int(max / 10)
 	for i in range(0, loop_size):
 		print("bullshit",)
+
 
 def one_tenth_print_newline(bullshit):
 	"""
@@ -141,6 +158,7 @@ def one_tenth_print_newline(bullshit):
 	loop_size = max / 10
 	for i in range(0, loop_size):
 		print("bullshit")
+
 
 def print_banner():
 	"""
